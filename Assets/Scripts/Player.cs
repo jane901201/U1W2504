627af9825d4627612d2dcde66951da0fe0d1c10a
jane901201 <75302011+jane901201@users.noreply.github.com>
2025-04-16 18:56:43 +0900

    キャラクターの移動方法を変更し、キャラクターおよびマップ画像を導入

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,8 +12,14 @@ public class Player : MonoBehaviour
     private float moveCooldown = 0.2f; // à⁄ìÆä‘äuÅiïbÅj
     private float lastMoveTime = 0f;
 
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         playerPos = new Vector3Int(0, 0, 0);
         Vector3 worldPosition = tilemap.CellToWorld(playerPos);
         gameObject.transform.position = worldPosition;
@@ -21,8 +27,28 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        // ì¸óÕéÊìæ
+        float h = Input.GetAxisRaw("Horizontal"); // AÇ∆D
+        float v = Input.GetAxisRaw("Vertical");   // WÇ∆S
+        movement = new Vector2(h, v).normalized;
+    }
 
+    private void FixedUpdate()
+    {
+        // é¿ç€ÇÃà⁄ìÆèàóù
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void MoveToCell(Vector3Int cellPos)
+    {
+        Vector3 worldPos = tilemap.CellToWorld(cellPos) + tilemap.cellSize / 2;
+        transform.position = worldPos;
+    }
+
+    private void TileMapMove()
+    {
         if (Time.time - lastMoveTime < moveCooldown) return;
+
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -48,12 +74,6 @@ public class Player : MonoBehaviour
             MoveToCell(playerPos);
             lastMoveTime = Time.time;
         }
-    }
-
-    void MoveToCell(Vector3Int cellPos)
-    {
-        Vector3 worldPos = tilemap.CellToWorld(cellPos) + tilemap.cellSize / 2;
-        transform.position = worldPos;
     }
 }
 
