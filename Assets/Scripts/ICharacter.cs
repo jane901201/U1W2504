efd0bed80d68fee0076344b8962bfd49a5e4f1e1
jane901201 <75302011+jane901201@users.noreply.github.com>
2025-04-19 00:15:14 +0900

    ICharacterマージ

--- Assets/Scripts/ICharacter.cs
+++ Assets/Scripts/ICharacter.cs
@@ -6,7 +6,18 @@ namespace DefaultNamespace
     {
         [SerializeField] private int hp = 3;
         [SerializeField] private Animator animator;
+        private CharacterState characterState; 
 
+        public CharacterState CharacterState { get => characterState; set => characterState = value; }
+
+        
         public int Hp { get => hp; set => hp = value; }
+        public Animator Animator{ get => animator; set => animator = value; }
+        
+        private void Awake()
+        {
+            characterState = new CharacterState();
+        }
+
     }
 }
