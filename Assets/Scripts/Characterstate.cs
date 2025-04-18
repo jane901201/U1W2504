a127ad3c1e2ec884aa15ad921ed4c91ac8dcc3e0
evyde <life.hanfeng@hotmail.com>
2025-04-18 23:15:17 +0900

    Change CharacterState and add some items.

--- Assets/Scripts/Characterstate.cs
+++ Assets/Scripts/Characterstate.cs
@@ -1,5 +1,21 @@
-public enum CharacterState
+public class CharacterState
 {
-    Love = 0,
-    Sad = 1 
+    public enum EmotionType
+    {
+        Love = 0,
+        Sad = 1
+    }
+
+    public enum RoleType
+    {
+        Oni = 67123, 
+        Human = 67124
+    }
+    
+    private EmotionType emotion = EmotionType.Love;
+    private RoleType role = RoleType.Human;
+
+    public EmotionType Emotion { get => emotion; set => emotion = value; }
+    public RoleType Role { get => role; set => role = value; }
+
 }
