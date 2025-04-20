7dfec24095386998620b721268c1358496ba3697
jane901201 <75302011+jane901201@users.noreply.github.com>
2025-04-20 14:52:46 +0900

    GameOverSceneやVictorySceneのBGMを更新

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -18,7 +18,7 @@ namespace DefaultNamespace
         [SerializeField] private GameObject[] objectsToSpawn;
         [SerializeField] private GameState gameState;
         [SerializeField] private SceneManager sceneManager;
-        
+        [SerializeField] private AudioSource audioSource;
         
         private List<Vector3Int> tilePositions = new List<Vector3Int>();
         private List<Vector3Int> obstaclePositions = new List<Vector3Int>();
